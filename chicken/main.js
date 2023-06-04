72c045e6bc5897ab30994124007df0b1eadf93cc
linh <linhlinhkook00@gmail.com>
2023-06-05 00:11:17 +0700

    Mini game | Dinosaur | demo v1

---
+++ chicken/main.js
@@ -0,0 +1,54 @@
+const query = document.querySelector.bind(document)
+const queryAll = document.querySelectorAll.bind(document)
+
+const PLATE_QUANTITY = 25
+
+const plateList = queryAll('.plates .ul-grand-parent .ul-parent ul li')
+
+const openedPlate = []
+
+plateList.forEach((plate, index) => {
+    plate.onclick = () => {
+        handleOpenPlate(plate, index)
+    }
+})
+
+
+const handleRandom = () => {
+    return Math.floor(Math.random() * 2);
+}
+
+const handleFakeOpen = (plate) => {
+    plate.classList.add('openning')
+
+    plate.classList.add('lost-opened')
+    if(handleRandom() === 1){
+        plate.classList.add('chicken')
+    }else{
+        plate.classList.add('bone')
+    }
+}
+
+const randomPlatesLeft = () => {
+    plateList.forEach((plate, index) => {
+        if(!openedPlate.includes(index)){
+            handleFakeOpen(plate)
+        }
+    })
+}
+
+// handle
+const handleOpenPlate = (plate, index) => {
+    plate.classList.add('openning')
+
+    plate.classList.add('lost-opened')
+    if(handleRandom() === 1){
+        plate.classList.add('chicken')
+        openedPlate.push(index)
+    }else{
+        plate.classList.add('bone')
+        randomPlatesLeft()
+    }
+
+    console.log(openedPlate)
+}
